4386454eafa0403ab500bd2e31584c1208569eeb
szymon <szymonkaminski.ptb@gmail.com>
2017-12-23 23:55:32 +0100

    add mvp classes

---
+++ app/src/main/java/com/shhatrat/boilerplate_kkmvp/di/baseUi/android/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.shhatrat.boilerplate_kkmvp.di.baseUi.android
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by szymon on 23/12/17.
+ */
+abstract class BaseActivity: AppCompatActivity(), BasePresenterHandle{
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        attachPresenter()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        detachPresenter()
+    }
+}
