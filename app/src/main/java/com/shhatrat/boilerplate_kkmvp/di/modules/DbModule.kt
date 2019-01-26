f3dc7c517bdfd36cd21cf0f95c412a108796cf92
szymon <szymonkaminski.ptb@gmail.com>
2019-01-26 19:44:55 +0100

    fix merge fix di

--- app/src/main/java/com/shhatrat/boilerplate_kkmvp/di/modules/DbModule.kt
+++ app/src/main/java/com/shhatrat/boilerplate_kkmvp/di/modules/DbModule.kt
@@ -4,13 +4,13 @@ import com.shhatrat.boilerplate_kkmvp.data.repository.realm.DbRepository
 import com.shhatrat.boilerplate_kkmvp.data.repository.realm.DbRepositoryImpl
 import io.realm.Realm
 import org.koin.dsl.module.Module
-import org.koin.dsl.module.applicationContext
+import org.koin.dsl.module.module
 
 /**
  * Created by szymon on 6/01/18.
  */
-val dbModule: Module = applicationContext {
-        bean { getRealm() }
-        bean { DbRepositoryImpl() } bind DbRepository::class
+val dbModule: Module = module {
+        single { getRealm() }
+        single { DbRepositoryImpl() } bind DbRepository::class
     }
     fun getRealm() = Realm.getDefaultInstance()
